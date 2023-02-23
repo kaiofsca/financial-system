c1b6ce69a6becb58d3f95f52d82c8f427ac4b3e3
Kaio Fonseca <mkaiofonseca@gmail.com>
2023-02-23 12:08:46 -0300

    estrutura inicial

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    { date: new Date(2023, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2023, 9, 15), category: 'food', title: 'Bobs', value: 2.50 },
+    { date: new Date(2023, 9, 18), category: 'rent', title: 'Aluguel Apt', value: 800 },
+    { date: new Date(2023, 10, 16), category: 'salary', title: 'Salário', value: 2500 },
+];
