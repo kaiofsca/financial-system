aa05731adda1d1f85dd319f940e3f9f54a2ea77b
Kaio Fonseca <mkaiofonseca@gmail.com>
2023-02-24 15:28:39 -0300

    criando dataFilter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { items } from './data/items';
+import { categories } from './data/categories';
+import { getCurrentMonth } from './helpers/dateFilter'
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth);
+
+  useEffect(() => {
+    
+  }, [list, currentMonth])
+
   return (
     <C.Container>
       <C.Header>
---
+++ src/helpers/dateFilter.ts
@@ -0,0 +1,13 @@
+// TUDO RELACIONADO A MANIPULÇÃO DE DATAS ESTA AQUI
+import { Item } from '../types/Item';
+
+export const getCurrentMonth = () => {
+    let now = new Date();
+    return `${now.getFullYear()}-${now.getMonth()+1}`;
+}
+
+export const filterListByMonth = (list: Item[], date: string): Item[] => {
+    let newList: Item[] = [];
+
+    return newList;
+}
