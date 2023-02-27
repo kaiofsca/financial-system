d356d841e9cc08264575807401f0ede027d832cf
Kaio Fonseca <mkaiofonseca@gmail.com>
2023-02-27 16:25:02 -0300

    continuando

--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -8,6 +8,16 @@ export const getCurrentMonth = () => {
 
 export const filterListByMonth = (list: Item[], date: string): Item[] => {
     let newList: Item[] = [];
+    let [year, month] = date.split('-');
+
+    for(let i in list) {
+        if( // o ano e o mês do intem tem q bater com o que eu quero filtrar
+            list[i].date.getFullYear() === parseInt(year) &&
+            (list[i].date.getMonth() + 1) === parseInt(month)
+        ) {
+            newList.push(list[i]);
+        }
+    }
 
     return newList;
 }
