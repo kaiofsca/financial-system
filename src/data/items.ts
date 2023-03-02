05c13d65f40281477a02370bf9158e3d962dff87
Kaio Fonseca <mkaiofonseca@gmail.com>
2023-03-02 16:27:42 -0300

    a

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,8 @@
 import { Item } from '../types/Item'
 
 export const items: Item[] = [
-    { date: new Date(2023, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
-    { date: new Date(2023, 9, 15), category: 'food', title: 'Bobs', value: 2.50 },
-    { date: new Date(2023, 9, 18), category: 'rent', title: 'Aluguel Apt', value: 800 },
-    { date: new Date(2023, 10, 16), category: 'salary', title: 'Salário', value: 2500 },
+    { date: new Date(2023, 3, 15), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2023, 3, 15), category: 'food', title: 'Bobs', value: 2.50 },
+    { date: new Date(2023, 3, 18), category: 'rent', title: 'Aluguel Apt', value: 800 },
+    { date: new Date(2023, 4, 16), category: 'salary', title: 'Salário', value: 2500 },
 ];
